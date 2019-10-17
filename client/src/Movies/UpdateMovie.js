54ffc0b5c1d1e45e5f1ae5b59399a413c15122aa
AjileJ <53097022+AjileJ@users.noreply.github.com>
2019-10-17 16:26:53 -0700

    unfinished

--- client/src/Movies/UpdateMovie.js
+++ client/src/Movies/UpdateMovie.js
@@ -1,31 +1,35 @@
 import React, {useState} from 'react';
 import axios from 'axios';
 
-const initialItem = {
-  title: '',
-  director: '',
-  metascore: '',
-  stars: []
-}
-
-
 
 const UpdateMovie = props => {
+  const initialItem = {
+    id: props.match.params.id,
+    title: '',
+    director: '',
+    metascore: '',
+    stars: []
+  }
  
   const [update, setUpdate] = useState(initialItem);
 
-  const handleChanges = ev => {
-    ev.preventDefault();
-    setUpdate({...update, [ev.target.name]:ev.target.value})
+  const handleChanges = event => {
+    console.log(update.title);
+    event.preventDefault();
+    setUpdate({...update, [event.target.name]:event.target.value})
   }
  
 
   const handleSubmit = event => {
+    const id = props.match.params.id;
+    console.log(id);
     event.preventDefault();
+    console.log(update)
     axios
-    .put(`http://localhost:5000/update-movie/${update.id}`, update)
+    .put(`http://localhost:5000/api/movies/${id}`, update)
     .then(res => {
       console.log(res)
+
     })
     .catch(err => {
       console.log(err.response)
@@ -37,11 +41,13 @@ const UpdateMovie = props => {
       <h1>Update Items</h1>
       <form onSubmit={handleSubmit}>
         <input type = 'text' name = 'title' onChange = 
-        {handleChanges} value = {update.title}/>
-        <input type = 'text' name = 'director' onChange = {handleChanges} value = {update.director}/>
-        <input type = 'number' name = 'metascore' onChange = {handleChanges} value = {update.metascore}/>
-        <input type = 'array' name = 'stars' onChange = 
-        {handleChanges} value = {update.stars}/>
+        {e => handleChanges(e)} value = {update.title}/>
+        <input type = 'text' name = 'director' onChange = 
+        {e => handleChanges(e)} value = {update.director}/>
+        <input type = 'number' name = 'metascore' onChange =
+        {e => handleChanges(e)} value = {update.metascore}/>
+        {/* <input type = 'array' name = 'stars' onChange = 
+        {e => handleChanges(e)} value = {update.stars}/> */}
         <button className = 'form'>Update Movie</button>
       </form>
     </div>
